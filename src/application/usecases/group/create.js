b4ec73df1664444fbb51bc6d6104b98032cc3953
Robert <roberto88526@gmail.com>
2025-08-06 16:57:45 -0300

    feat: group use case updated.
    Now with a more detailed creation, the group now has a new structure.

--- src/application/usecases/group/create.js
+++ src/application/usecases/group/create.js
@@ -1,5 +1,6 @@
 const { Message } = require("../../../common/message");
 const { Group } = require("../../../domain/entities/Group");
+const { RNG } = require("../../utils/RNG");
 
 class GroupCreateUseCase {
     #_repository;
@@ -13,6 +14,18 @@ class GroupCreateUseCase {
         if (group) return Message.failure("Grupo não encontrado.");
 
         const newGroup = new Group(request.session, request.name);
+        const { difficulty, number } = Group.difficultyGenerator();
+
+        const randFlag = newGroup[difficulty][RNG(0, newGroup[difficulty].length)];
+
+        newGroup.current_info.flag_name = randFlag.name;
+        newGroup.current_info.flag_img = randFlag.img;
+        newGroup.current_info.flags_difficulty = difficulty;
+        newGroup.current_info.alternatives = Group.alternativesGenerator(
+            newGroup[difficulty],
+            newGroup.current_info.flag_name);
+        newGroup.level = number;
+        newGroup.moves += 1;
 
         await this.#_repository.add(newGroup);
 
