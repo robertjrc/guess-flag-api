0e3f758c33e2eaa36f7ad48ab825a03e8a56fe50
Robert <roberto88526@gmail.com>
2025-08-08 22:07:32 -0300

    test(groupCrate) All saved.
    Now we can save the created group.

--- test/group/create.test.js
+++ test/group/create.test.js
@@ -1,4 +1,5 @@
 const { GroupCreateUseCase } = require("../../src/application/usecases/group/create");
+const { GroupSaveChangesUseCase } = require("../../src/application/usecases/group/save");
 const { GroupInMemoryRepository } = require("../../src/infrastructure/repositories/groupInMemoryRepository");
 
 const id = "930202030"
@@ -14,7 +15,8 @@ test("should create a group", async () => {
     const createGroupUseCase = new GroupCreateUseCase(repository);
     const response = await createGroupUseCase.execute(data);
 
-    console.log(response);
+    const groupSaveUsecase = new GroupSaveChangesUseCase(repository);
+    await groupSaveUsecase.execute(id, response.value);
 
     expect(response.success).toBe(true);
 });
