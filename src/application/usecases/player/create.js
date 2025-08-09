b02dd0a6e3b5469a715b7fc6c5c350beca59ff96
Robert <roberto88526@gmail.com>
2025-08-09 13:51:43 -0300

    refactor(playerCreate): Player verify removed.
    It is no longer necessary to check if the player already exists.

--- src/application/usecases/player/create.js
+++ src/application/usecases/player/create.js
@@ -1,20 +1,9 @@
-const { Message } = require("../../../common/message");
 const { Player } = require("../../../domain/entities/Player");
 
 class PlayerCreateUseCase {
-    #_repository;
-
-    constructor(repository) {
-        this.#_repository = repository;
-    };
-
     execute(request) {
-        const player = this.#_repository.getById(request.id, request.group);
-        if (player) return Message.failure("Jogador já existe.");
-
         const newPlayer = new Player(request.id, request.name);
-
-        return Message.success(`${newPlayer.name} foi criado com sucesso.`, newPlayer);
+        return newPlayer;
     };
 }
 
