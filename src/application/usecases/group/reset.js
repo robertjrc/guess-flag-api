0d694fc62b3284adbd7438791d2976cfa7793e96
Robert <roberto88526@gmail.com>
2025-08-09 13:00:02 -0300

    feat(groupReset): Group reset.
    Now the group can go back to square one.

---
+++ src/application/usecases/group/reset.js
@@ -0,0 +1,25 @@
+const { Message } = require("../../../common/message");
+const { Group } = require("../../../domain/entities/Group");
+const { RNG } = require("../../utils/RNG");
+
+class GroupResetUseCase {
+    async execute(request) {
+        const newGroup = new Group(request.id, request.name);
+        const { difficulty, number } = Group.difficultyGenerator();
+
+        const randFlag = newGroup[difficulty][RNG(0, newGroup[difficulty].length)];
+
+        newGroup.current_info.flag_name = randFlag.name;
+        newGroup.current_info.flag_img = randFlag.img;
+        newGroup.current_info.flags_difficulty = difficulty;
+        newGroup.current_info.alternatives = Group.alternativesGenerator(
+            newGroup[difficulty],
+            newGroup.current_info.flag_name);
+        newGroup.level = number;
+        newGroup.moves += 1;
+
+        return Message.success("Grupo criado com sucesso.", newGroup);
+    };
+}
+
+module.exports = { GroupResetUseCase };
